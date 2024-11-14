73924cef772215ceed59ca42dc135bafdccc85b2
Chander Pal <44088072+chander-pal@users.noreply.github.com>
2024-11-14 15:48:41 +0000

    Fixed Tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,9 +5,9 @@ from example import add, multiply
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(0, 0) == 1
+    assert add(0, 0) == 0
 
 def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(-1, 5) == -5
-    assert multiply(0, 10) == 01
+    assert multiply(0, 10) == 0
